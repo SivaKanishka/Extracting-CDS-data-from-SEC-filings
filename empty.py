d5549c4afe9f312e8a90898306d46720726f1c27
Siva Kanishka <kanishka.siva@gmail.com>
2017-09-07 17:40:58 +0530

    Add files via upload

---
+++ empty.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 04 15:28:23 2017
+
+@author: Chebolu
+"""
+import sqlite3
+import pandas as pd
+import re
+import numpy as np
+from bs4 import BeautifulSoup
+
+conn = sqlite3.connect('edgar_idx.db')
+c = conn.cursor()
+
+c.execute('select * from idx where type = ? or type like ?', ('N-Q', '%CSR%'))
+output1 = c.fetchall()
+
+
+labels = ['CIK', 'Company_Name', 'Form_Type', 'Date', 'Filename']
+input = pd.DataFrame.from_records(output1, columns=labels)
+prefix = "https://www.sec.gov/Archives/"
+input = input[['CIK', 'Company_Name', 'Date', 'Filename', 'Form_Type']]
+input['URL'] = prefix + input[['Filename']]
+
+from sklearn.preprocessing import LabelEncoder
+
+input["ID"] = LabelEncoder().fit_transform(input.CIK)
+input = input[['ID', 'CIK', 'Company_Name', 'Date', 'Filename', 'Form_Type', 'URL']]
+
+dummy = pd.DataFrame(columns = ['ID', 'CIK', 'Company_Name', 'Date', 'Filename', 'Form_Type', 'URL', 'Attributes', 'Values'])
+good_indices = []
+url_table_index = []
+
+import time
+
+
+for u in range(10000, 20000):
+    ID = input['ID'][u]
+    cik = input['CIK'][u]
+    comp_name = input['Company_Name'][u]
+    date = input['Date'][u]
+    filename = input['Filename'][u]
+    form_type = input['Form_Type'][u]
+    url = input['URL'][u]
+    with open(input.at[u,'Filename'].replace('/', '_'), 'r') as f:
+        soup = BeautifulSoup(f)
+    all_tables = soup.find_all('table')
+    x = []
+    sum = 0
+    for m in range(0, len(all_tables)):
+        for row_value in all_tables[m].find_all('tr'):
+            cds_columns = row_value.find_all('td')
+            for cds_column in cds_columns:
+                if cds_column.get_text().strip() not in ['Credit Default Swap', 'CDS', 'Credit Swap', 'Credit Default Swaps']:
+                    break
+                elif cds_column.get_text().strip() in ['Credit Default Swap', 'CDS', 'Credit Default Swaps', 'Credit Swap']:
+                    url_table_index.append((u,m))
+                    good_indices.append(u)
+                    sum +=1
+    if sum == 0:
+        x.append(np.nan)
+        dummy = dummy.append(pd.DataFrame.from_items([('ID', ID), ('CIK', cik), ('Company_Name', comp_name), ('Date', date), ('Filename', filename), ('Form_Type', form_type), ('URL', url), ('Attributes', x), ('Values', x)]))
+    print(u)
+
+
+import pickle
+
+with open("z_url_table_index.txt", "wb") as fp:   #Pickling
+    pickle.dump(url_table_index, fp)
+
+with open("z_final_indices.txt", "wb") as fp:   #Pickling
+    pickle.dump(final_indices, fp)
+
+with open("z_url_table_index.txt", "rb") as fp:   # Unpickling
+    url_table_index = pickle.load(fp)
+
+with open("z_final_indices.txt", "rb") as fp:   # Unpickling
+    final_indices = pickle.load(fp)
---
+++ final.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 07 01:26:14 2017
+
+@author: Siva Kanishka
+"""
+import pandas as pd
+import numpy as np
+import re
+
+final = pd.read_excel(open('output1_10000.xlsx','rb'), sheetname='Sheet1')
+
+qf = final.dropna()
+
+uniq_val = list(set(qf['Attributes']))
+
+qf['Attributes'] = qf.Attributes.replace('Description', 'CDS Text')
+
+qf = qf[qf.Attributes == 'CDS Text']
+
+qf = qf[['ID', 'CIK', 'Company_Name', 'Date', 'Filename', 'Form_Type', 'URL', 'Values']]
+
+qf = qf.rename(columns = {'Values':'CDS_Text'})
+
+cds = list(qf['CDS_Text'])
+
+z = re.compile(r"default(.+)? of ([A-Z][A-Za-z]+,? of ([A-Z][A-Za-z]+.?,? ?)+|[A-Z][A-Za-z]+,? ([A-Z][A-Za-z]+.?,? ?)+)")
+count = re.compile(r"([A-Z][A-Za-z]+,? of ([A-Z][A-Za-z]+.?,? ?)+|[A-Z][A-Za-z]+,? ([A-Z][A-Za-z]+.?,? ?)+)")
+
+# Underlying identification
+under = []
+for i in range(0,len(cds)):
+    text = cds[i]
+    text = text.strip().replace('\n', ' ')
+    (start1, end1) = z.search(text).span()
+    text = text[start1:end1]
+    (start2, end2) = count.search(text).span()
+    under.append(text[start2:end2].strip().strip(','))
+    print i
+
+# Actual underlying
+reg_actual = re.compile(r"(\d+[^A-Za-z]+% [a-z]+ [^A-Za-z]+/\d\d|\d+[^A-Za-z]+% [^A-Za-z]+/\d\d)")
+actual_underlying = []
+for j in range(0,len(cds)):
+    text = cds[j]
+    text = text.strip().replace('\n', ' ')
+    actual = reg_actual.search(text)
+    if actual != None:
+        actual_underlying.append(under[j] + ' ' + text[actual.start():actual.end()])
+    else:
+        actual_underlying.append(under[j] + ' ' + 'default of issues')
+    print(j)
+
+# Counterparty identification
+counterparty = []
+for k in range(0,len(cds)):
+    counter = []
+    t = []
+    text = cds[k]
+    text = text.strip().replace('\n', ' ')
+    for match in re.finditer(r"([A-Z][A-Za-z]+,? of ([A-Z][A-Za-z]+.?,? ?)+|[A-Z][A-Za-z]+ ([A-Z][A-Za-z]+.?,? ?)+|pay [A-Z][A-Za-z]+,? upon)", text):
+        t = match.group()
+        t = t[:-1].strip().strip(',')
+        counter.append(t)
+    index = [counter.index(c) for c in counter if c != under[k]]
+    counterparty.append(counter[index[0]])
+    print(k)
+
+# CDS_Transaction identification
+buy = re.compile(r"((P|p)ay quarterly|(P|p)ay.+year(ly)?)")
+sell = re.compile(r"((R|r)eceive quarterly|(R|r)eceive (quarterly|annually|year(ly)?))")
+trans = []
+miss = []
+for l in range(0,len(cds)):
+    text = cds[l]
+    text = text.strip().replace('\n', ' ')
+    if buy.search(text) != None:
+        trans.append('BUY')
+    elif sell.search(text) != None:
+        trans.append('SELL')
+    else:
+       miss.append(l) 
+    print(l)
+
+# Appending columns to data frame
+qf['Underlying'] = under
+qf['Exact Underlying'] = actual_underlying
+qf['CounterParty'] = counterparty
+qf['CDS Transaction'] = trans
+
+# Writing data frame to an Excel workbook
+writer = pd.ExcelWriter('output2_10000.xlsx', engine='xlsxwriter')
+qf.to_excel(writer, sheet_name='Sheet1')
+writer.save()
